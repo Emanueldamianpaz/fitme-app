5858c2212b7d0bf63d9b5c44d17ed5b01ddb8b2f
Emanuel <emanueldamian.paz@gmail.com>
2019-10-06 12:03:47 -0300

    Begin add

--- src/main/resources/public/js/controllers/usersController.js
+++ src/main/resources/public/js/controllers/usersController.js
@@ -1,6 +1,9 @@
-fitme.controller('usersController', function ($rootScope) {
+fitme.controller('usersController', function ($rootScope, $scope, UsersService, MessageNotification, $filter) {
 
     $rootScope.stateCurrent = "users";
 
+    UsersService.getExercises().then(function (response) {
+        $scope.exerciseList = response.data;
+    });
 })
 
---
+++ src/main/resources/public/js/services/usersService.js
@@ -0,0 +1,39 @@
+fitme.service('UsersService', function ($http) {
+
+    var pathUsers = api + '/user';
+    var pathUsersInfo = api + '/user_info';
+
+    this.getListUsersInfo = function () {
+        return $http(getListUsersInfo());
+    }
+
+    function getListUsersInfo() {
+        return req = {
+            method: 'get',
+            url: pathUsersInfo
+        }
+    };
+
+    this.getDetailUser = function (id) {
+        return $http(getDetailUser(id));
+    }
+
+    function getDetailUser(id) {
+        return req = {
+            method: 'get',
+            url:  pathUsers + '/' + id + '/info',
+        }
+    };
+
+    this.sendMessage = function (message, id) {
+        return $http(sendMessage(message, id));
+    }
+
+    function sendMessage(message, id) {
+        return req = {
+            method: 'post',
+            url:  pathUsers + '/' + id + '/message',
+            data: message
+        }
+    };
+})
